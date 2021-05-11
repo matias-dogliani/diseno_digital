111944509388b885a96c519709384ef6e19008e0
matias_dogliani <matiasdogliani@hotmail.com>
2021-05-11 11:26:51 -0300

    Modulo top controlador

---
+++ top.v
@@ -0,0 +1,20 @@
+ 
+
+/*La particion de un diseno digital en modulos es importante */
+
+module top (
+    
+     input clock, 
+     input i_reset, 
+     input [3:0] i_sw,
+     output [3:0] o_led,
+     output [3:0] o_led_b,
+     output [3:0] o_led_g
+ 
+ );
+
+
+ endmodule  
+
+
+
