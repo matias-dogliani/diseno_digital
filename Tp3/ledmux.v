efc69dc746008f57894729cffaf9eee235fb1325
matias-dogliani <matiasdogliani@hotmail.com>
2021-06-15 19:07:17 -0300

    agrego ledmux

--- Tp3/ledmux.v
+++ Tp3/ledmux.v
@@ -1,24 +1,23 @@
+//Multiplexor doble con entradas de 4b
+
 module ledmux 
 #(
 	parameter N_LEDS = 4
 	parameter MUXSEL = 1	
-	parameter N_COLORS = 3
 )
 (
 	input [MUXSEL-1 : 0] mux_sel ,  			//btn[0] 
-	input [N_COLORS -1 : 0] i_btn,				// 0_led[3:1] 
-	input [N_LEDS - 1 : 0] i_led,				// LEDs de shift o Flash 
-	output [N_LEDS - 1 : 0] o_led,
-	output [N_LEDS - 1 : 0] o_led_r,
-	output [N_LEDS - 1 : 0] o_led_g,
-	output [N_LEDS - 1 : 0] o_led_b
-);
+	input [N_LEDS -1 : 0] shift_leds,
+	input [N_LEDS -1 : 0] flash_leds,  
+	output [N_LEDS - 1 : 0] o_leds,
+	output o_led_mux );
 
 	
-	// Pulsadores a LEDS indicativos 
-	assign o_led = i_btn ;
+  	localparam SHIFT = 1'b0 ; 
+  	localparam FLASH = 1'b1 ;
 
 
-
+  	assign o_leds = (mux_sel == SHIFT) ? shift_leds : flash_leds;
+ 	assign o_led_mux = mux_sel ;
 
 endmodule //ledmux
