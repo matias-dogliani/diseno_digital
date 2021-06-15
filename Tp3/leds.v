49fd39748ecfe4979e6903f6badd161ef5569663
matias-dogliani <matiasdogliani@hotmail.com>
2021-06-15 19:06:49 -0300

    Termino de describir submodulos

--- Tp3/leds.v
+++ Tp3/leds.v
@@ -1,4 +1,40 @@
-// Modulo con los 3 bloques de LEDS. Recibe el arreglo de LEDs, El boton de enable y ese asociado a otro LED 
-//assign salida if enable = entrada 
+// Bloque de LEDs 
 
-//FF conectado a un mux para guardar el estado del pulsador 
+module leds 
+#(
+	parameter N_LEDS = 4
+	parameter COLOR = 3
+)
+(
+	input [N_LEDS -1 : 0] i_led, 		// Valores de LEDs (de FLASH o de SHIFT)
+	input [COLOR -1:0] i_btn,			// Botones de seleccion de colores 					 
+	output [COLOR -1:0] o_led,			// LEDs de seleccion de colores  
+	output [N_LEDS -1 : 0] o_led_r,		// LEDs rojos 
+	output [N_LEDS -1 : 0] o_led_g,
+	output [N_LEDS -1 : 0] o_led_b,
+);
+	assign o_led = i_btn;   //Los LEDs indicativos corresponden con los btn de seleccion de color 
+
+	reg [N_LEDS -1 : 0] led_r;
+	reg [N_LEDS -1 : 0] led_g;
+	reg [N_LEDS -1 : 0] led_b;
+
+always @(posedge clk ) begin 				
+
+	case (i_btn)
+		3'b100  : led_r <= i_led; 	
+		3'b010  : led_g <= i_led; 	
+		3'b001  : led_b <= i_led; 	
+		default : begin
+			led_r <= led_r;
+			led_g <= led_g;
+			led_b <= led_b;
+		end
+			
+	endcase
+
+end
+
+
+
+endmodule //leds
