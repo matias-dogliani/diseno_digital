a47bb9574eca025ed78a383653ed7f4beda3606f
matias-dogliani <matiasdogliani@hotmail.com>
2021-07-06 19:25:01 -0300

    Saturacion y trunc

---
+++ Tp4/Verilog/README.md
@@ -0,0 +1,22 @@
+ # Trabajo práctico Nº 4
+
+ ## Ejercicio 1 
+
+ Link a resolucion notebook 
+ Alguna explicacion 
+
+
+
+ ## Ejercicio 2 
+
+  ### Full Resolution 
+
+  Algo de explicacion 
+  Hardware 
+
+  ### Saturacion y truncado  
+
+
+  ### Saturación y redondeo 
+
+  
--- Tp4/Verilog/sum.v
+++ Tp4/Verilog/sum.v
@@ -1,8 +1,8 @@
-module sumFp(
+module sumFp
 #(
 	// Formato de A 
-	parameter NB_IN_A =16 							//Bit totales 
-	parameter NBF_IN_A =14							//Bits fraccionales 	
+	parameter NB_IN_A =16, 							//Bit totales 
+	parameter NBF_IN_A =14,							//Bits fraccionales 	
 	parameter NBI_IN_A =NB_IN_A - NBF_IN_A,  		//Bit enteros  
 	
 	// Formato de B 	
@@ -13,38 +13,62 @@ module sumFp(
 	// Salida	
 	parameter NB_OUT = 11, 
 	parameter NBF_OUT = 10,
-	parameter NBI_OUT = (NB_OUT > NBF_OUT)? NB_OUT-NBF_OUT : 0;  //Test . Sino NB_OUT - NBF_OUT solo
+	parameter NBI_OUT = (NB_OUT > NBF_OUT)? NB_OUT-NBF_OUT : 0,  //Test . Sino NB_OUT - NBF_OUT solo
 	
-
+	// Formato salida FR 
+	parameter NBI_O_FR = ( NBI_IN_A  > NBI_IN_B  )? NBI_IN_A + 1 : NBI_IN_B + 1 ,    //+1 para Carry 
+    parameter NBF_O_FR = ( NBF_IN_A > NBF_IN_B )? NBF_IN_A    : NBF_IN_B    ,
+    parameter NB_O_FR = NBI_O_FR + NBF_O_FR 
 )
+(
 	// Entradas 
     input  [NB_IN_A - 1:0] i_A,
     input  [NB_IN_B - 1:0] i_B,
-
-
+	
 	//Salidas 
-
-	output [NB_OUT -1 : 0]	o_sumS_11_10_ov_t, 
+	output [NB_O_FR - 1 : 0] o_sumFR,
+	output [NB_OUT -1 : 0]	o_sumS_ov_t
 );
 
-	// Formato salida FR 
-    localparam NBI_O_FR = ( NBI_IN_A  > NBI_IN_B  )? NBI_IN_A + 1 : NBI_IN_B + 1 ;    //+1 para Carry 
-    localparam NBF_O_FR = ( NBF_IN_A > NBF_IN_B )? NBF_IN_A    : NBF_IN_B    ;
-    localparam NB_O_FR = NBI_O_FR + NBF_O_FR ;
+	
 
 
     wire signed [NB_IN_A -1     : 0] a 	;   //Entrada signada 
     wire signed [NB_IN_B -1     : 0] b 	;   // Entrada b signada
-	wire signed [NB_O_FR-1  	: 0] sum;   //suma full res
-
+	wire signed [NB_O_FR-1  	: 0] sumFR;   //suma full res
+	reg  signed [NB_OUT - 1 : 0]  sumS_trunc_sat;
+	
+	
 	assign a = i_A;
 	assign b = i_B;
 
 	/*Suma Full Resolution*/
-	assign [NB_O_FR-1  	: 0] sum = a + b; 
+	//Acomo el punto porque son de diferente formato 
+	assign sumFR = (NBF_IN_A >= NBF_IN_B)?                           
+                a + $signed( {b, { NBF_IN_A - NBF_IN_B {1'b0}}}):
+                b + $signed( {a, { NBF_IN_B - NBF_IN_A {1'b0}}});
+    
+
+	/* Salifa Full Resolution */
+	assign o_sumFR = sumFR; 
 	
-	/*S(11.10) con overflow y truncado */	
-	assign o_sumS_11_10_ov_t = sum[NB_FULL_RES -1 - (NBI_O_FR- NBI_OUT) -:NB_OUT]	
+	/* Overflow y Truncado */	
+	assign o_sumS_ov_t = sumFR[NB_O_FR-1 - (NBI_O_FR- NBI_OUT) -:NB_OUT];	
+
+	/*Truncado y Saturacion */
+  	always @(*) begin
+        if ( &sumS_trunc_sat[(NB_FR -1)-:(NBI_FR-NBI_OUT)+1] || ~|sumS_trunc_sat[(NB_FR -1)-: (NBI_FR-NBI_OUT)+1])
+            sumS_trunc_sat_trn_sat = sumS_trunc_sat[ (NB_FR -1) - (NBI_FR - NBI_OUT) -: NB_OUT ];
+
+        else if ( sumS_trunc_sat[(NB_FR -1)] )
+            sumS_trunc_sat_trn_sat = { 1'b1 , { NB_OUT-1 {1'b0} }};
+
+        else
+            sumS_trunc_sat_trn_sat = { 1'b0 , { NB_OUT-1 {1'b1} }};
+    end
+
+	
+
 
 
 endmodule //sum
