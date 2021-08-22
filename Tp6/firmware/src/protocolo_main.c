376e1580902372e2d0581293aaa8c396f1d21eb1
matias-dogliani <matiasdogliani@hotmail.com>
2021-08-22 20:20:53 -0300

    Recepcion y validación funciona

--- Tp6/firmware/src/protocolo_main.c
+++ Tp6/firmware/src/protocolo_main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 #include "xparameters.h"
@@ -8,83 +7,102 @@
 #include "xuartlite.h"
 #include "microblaze_sleep.h"
 
-#define PORT_IN	 		XPAR_AXI_GPIO_0_DEVICE_ID //XPAR_GPIO_0_DEVICE_ID
-#define PORT_OUT 		XPAR_AXI_GPIO_0_DEVICE_ID //XPAR_GPIO_0_DEVICE_ID
+#define PORT_IN         XPAR_AXI_GPIO_0_DEVICE_ID //XPAR_GPIO_0_DEVICE_ID
+#define PORT_OUT        XPAR_AXI_GPIO_0_DEVICE_ID //XPAR_GPIO_0_DEVICE_ID
 
 //Device_ID Operaciones
-#define def_SOFT_RST            0
-#define def_ENABLE_MODULES      1
-#define def_LOG_RUN             2
-#define def_LOG_READ            3
+#define d_READ 1
+#define d_WRITE 0
 
+/*Trama structure*/
+#define FRAME_INIT  0x05               // (00000101)
+#define FRAME_HEAD_L  4
+#define FRAME_END  0x02
 XGpio GpioOutput;
 XGpio GpioParameter;
 XGpio GpioInput;
-u32 GPO_Value;
-u32 GPO_Param;
+u32 GPO_Value=0x00000000;
+u32 GPO_Param=0x00000000;
 XUartLite uart_module;
 
-//Funcion para recibir 1 byte bloqueante
-//XUartLite_RecvByte((&uart_module)->RegBaseAddress)
+unsigned char receiveFrame (unsigned char *frame);
+
+
 
 int main()
 {
-	init_platform();
-	int Status;
-	XUartLite_Initialize(&uart_module, 0);
 
-	GPO_Value=0x00000000;
-	GPO_Param=0x00000000;
-	unsigned char cabecera[4];
 
-	Status=XGpio_Initialize(&GpioInput, PORT_IN);
-	if(Status!=XST_SUCCESS){
+    unsigned char * frame;
+    unsigned char datos[] = "env";
+
+    init_platform();
+
+    /*      GPIO / UART Init*/
+
+    XUartLite_Initialize(&uart_module, 0);
+
+	if(XGpio_Initialize(&GpioInput, PORT_IN)!=XST_SUCCESS){
         return XST_FAILURE;
     }
-	Status=XGpio_Initialize(&GpioOutput, PORT_OUT);
-	if(Status!=XST_SUCCESS){
+
+	if(XGpio_Initialize(&GpioOutput, PORT_OUT)!=XST_SUCCESS){
 		return XST_FAILURE;
 	}
 	XGpio_SetDataDirection(&GpioOutput, 1, 0x00000000);
 	XGpio_SetDataDirection(&GpioInput, 1, 0xFFFFFFFF);
 
-	u32 value;
-  unsigned char datos;
+
+
+
 	while(1){
-    read(stdin,&cabecera[0],1);
 
-    /* unsigned char datos_rec[3]; */
-    /* short int i; */
-    /* for(i=0;i<3;i++){ */
-    /*   datos_rec[i]=XUartLite_RecvByte((&uart_module)->RegBaseAddress); */
-    /* } */
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// ACA es donde se escribe toda la funcionalidad
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        if (receiveFrame(frame))
+	    	{
 
-		   switch(cabecera[0]){
-           case '0':
-               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000249);
-               break;
-           case '1':
-               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000492);
-               break;
-           case '2':
-               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000924);
-               break;
-           case '3':
-               XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000000);
-               value = XGpio_DiscreteRead(&GpioInput, 1);
-               datos=(char)(value&(0x0000000F));
-               while(XUartLite_IsSending(&uart_module)){}
-               XUartLite_Send(&uart_module, &(datos),1);
-               break;
+           XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000249);
+
+		//	if (*device)
+        //       XGpio_DiscreteWrite(&GpioOutput,1, (u32) 0x00000249);
+        //
+        //    else{
+        //       datos="err";
+        //       while(XUartLite_IsSending(&uart_module)){}
+        //       XUartLite_Send(&uart_module, &(datos),3);
+        //    }
+
 		   }
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// FIN de toda la funcionalidad
-//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
         }
-	
+
 	cleanup_platform();
 	return 0;
 }
+
+
+unsigned char receiveFrame(unsigned char *frame)
+{
+
+	 /*Check INIT Frame */
+	    if ( read(stdin, frame,1) && ((*frame >> 5) & 7 == FRAME_INIT ) ){
+
+	        //LEO LOS 3 bits restantes de la cabecera
+	        //Si es trama corta, leo el size y leo esas misma cantidad de bytes
+	        read(stdin,(frame+1),FRAME_HEAD_L - 1);
+
+
+	        if ( (*frame >> 4) & 1 == 0 ){       //Short frame
+
+	            read(stdin, (frame + FRAME_HEAD_L), 3);
+
+	            if ( read(stdin, (frame + FRAME_HEAD_L + 3),1) && \
+	                    *(frame + FRAME_HEAD_L + 3+1) >> 5 & 7 == FRAME_END)
+	                return 1;
+	        }
+
+	    }
+
+	    return 0;
+
+}
+
