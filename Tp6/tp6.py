9816dbc0ecd79488b068cd735978a7295e0f1350
matias-dogliani <matiasdogliani@hotmail.com>
2021-08-22 20:21:31 -0300

    Trama para escritura funcionando

--- Tp6/tp6.py
+++ Tp6/tp6.py
@@ -1,71 +1,57 @@
-"""
-Programa para enviar y recibir datos codificados segun la trama implementanda 
-en tp2 
-
-Poner esto en README: 
-Cabecera (4) 
-
-    Byte 1 : Init 3'b - Type 1'b - Size 4'b
-    Byte 2 
-    Byte 3 
-    Byte 4 : device (Write or Read en este caso) 
-
-data (2**16)
-
-
-Cola (1) 
-    
-    Byte 1 : End 3'b - Type 1'b - Size 4'b
-"""
-
 import serial 
 import sys
+
 def main(): 
     
-   # portName=sys.argv[1] if len(sys.argv)>1 else "dev/ttyVPA" 
-   # print(portName) 
-   # ser = serial.Serial( 
-   #         port = portName, 
-   #         baudrate=115200, 
-   #         parity=serial.PARITY_NONE, 
-   #         stopbits= serial.STOPBITS_ONE, 
-   #         bytesize= serial.EIGHTBITS) 
+    portName=sys.argv[0] if len(sys.argv)>1 else "/dev/ttyUSB6" 
+    print("Puerto: " , portName) 
+    
+    ser = serial.Serial( 
+            port = portName, 
+            baudrate=115200, 
+            parity=serial.PARITY_NONE, 
+            stopbits= serial.STOPBITS_ONE, 
+            bytesize= serial.EIGHTBITS) 
 
-   # if ser.isOpen() == False: raise ValueError('El puerto no se pudo abrir')
+    ser.timeout=None
+    ser.flushInput()
+    ser.flushOutput()
+
+    if ser.isOpen() == False: raise ValueError('El puerto no se pudo abrir')
 
     
     print("1- Prender LEDs: \n" ) 
-    print("\t < {0 1 2 3} >< {R G B} ><  {1 0}  >")
+    print("\t < |0| |1| |2| |3| >< |R| |G| |B| ><  |1| |0|  >")
     print("\t <  LEDs ID  ><  COLOR  ><  On/Off >")
-    
-    print('\n2- Para leer estado de los switch enviar:\n')
-    print('\t$ <switch ID>')
-    print('\t$ <[0 1 2 3]>')
-    
     print('\n3- Q para terminar \n')
 
 
     while 1: 
-
         data = input("\n Ingrese un comando: ")  
         
         if data.upper() == 'Q': 
-            #ser.close() 
+            ser.close() 
             exit('Programa finalizado \n')
 
         if len(data)==3: 
             trama=TramaEnconder(data,1) #device = 1 : Write GPIO  
-            print(trama) 
+            for byte in trama:
+                print("Enviado ",byte)
+                ser.write(byte)
 
         elif len(data)==1: 
            trama=TramaEnconder(data,0) 
            print(trama) 
 
 
-def TramaEnconder(data,device):                                                  
-                                                                                
-    trama = []                                                                  
+def TramaEnconder(datos,device):                                                  
     
+    
+    data = []                                                                      
+    for char in datos:                                                               
+        data.append(char.encode()) 
+
+    trama = []                                                                  
     if len(data) >16:   #Solo trama corta
         return -1                                                               
                                                                                 
@@ -79,7 +65,7 @@ def TramaEnconder(data,device):
     trama.append(bytes([device]))                                                    
                                                                                 
     for byte in data:                                                           
-        trama.append(byte)                                                      
+        trama.append(bytes(byte))                                                      
                                                                                 
     trama.append(fin_trama)                                                     
     return trama
